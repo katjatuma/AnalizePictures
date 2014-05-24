ba93e70bbf795d9d567df3865cfc5cd1b7d061e5
Ziga Stopinsek <sigi.kajzer@gmail.com>
2014-05-24 22:05:59 +0200

    updating prepare data script

--- image-processing/prepare_data.py
+++ image-processing/prepare_data.py
@@ -3,6 +3,8 @@ from optparse import OptionParser
 import json
 import os.path
 
+SUPPORTED_FILE_TYPES = ('jpg', 'png', )
+
 parser = OptionParser()
 parser.add_option('-a', '--author', dest='author', default=None,
                   help='Specify author to prepare data.')
@@ -13,9 +15,10 @@ if not options.author:
 	print 'Nothing to do. You have to type author id. Type -h or --help for help.'
 	exit(0)
 
-author_fname = os.path.join(options.data_dir, options.author + '.json')
+author_manifest = os.path.join(options.data_dir, options.author + '.json')
+author_work_dir = os.path.join(options.data_dir, options.author)
 author = None
-with file(author_fname, 'r') as fd:
+with file(author_manifest, 'r') as fd:
 	author = json.loads(fd.read())
 
 if not author:
@@ -23,5 +26,19 @@ if not author:
 	exit(1)
 
 for work in author['work']:
-	# do something with the pictures
-	pass
+	fname = os.path.join(author_work_dir, work['id'])
+	for ftype in SUPPORTED_FILE_TYPES:
+		if os.path.exists(fname + '.' + ftype):
+			fimg = fname + '.' + ftype
+			fjson = fname + '.json'
+			break
+	else:
+		print 'Manifest has non-existing work specified'
+		exit(1)
+
+	img_data = {}
+	# TODO: process image data
+
+	with file(fjson, 'w') as fd:
+		fd.write(json.dumps(img_data))
+exit(0)
