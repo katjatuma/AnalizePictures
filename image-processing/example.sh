13150e587466c4e382f221efec96eed326f1c4b6
Ziga Stopinsek <sigi.kajzer@gmail.com>
2014-05-25 14:45:26 +0200

    added script for optaining data from wiki and example, prepere data now prepares rgb channel data

---
+++ image-processing/example.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./grep_wiki.py -a brugel -w http://en.wikipedia.org/wiki/List_of_paintings_by_Pieter_Bruegel_the_Elder
--- image-processing/prepare_data.py
+++ image-processing/prepare_data.py
@@ -2,12 +2,13 @@
 from optparse import OptionParser
 import json
 import os.path
+from PIL import Image
 
 SUPPORTED_FILE_TYPES = ('jpg', 'png', )
 
-parser = OptionParser()
+parser = OptionParser(usage='usage: %prog [options] [work1 [work2 ... ]]')
 parser.add_option('-a', '--author', dest='author', default=None,
-                  help='Specify author to prepare data.')
+				  help='Specify author to prepare data.')
 parser.add_option('-d', '--data', dest='data_dir', default='../data/',
 				  help='Specify data directory (default: ../data/)')
 (options, args) = parser.parse_args()
@@ -25,20 +26,42 @@ if not author:
 	print 'Author doesn\'t exist'
 	exit(1)
 
-for work in author['work']:
-	fname = os.path.join(author_work_dir, work['id'])
-	for ftype in SUPPORTED_FILE_TYPES:
-		if os.path.exists(fname + '.' + ftype):
-			fimg = fname + '.' + ftype
-			fjson = fname + '.json'
-			break
-	else:
-		print 'Manifest has non-existing work specified'
-		exit(1)
+def hexencode(rgb):
+	return '#%02x%02x%02x' % rgb
+
+for work in author['works']:
+	fjson = os.path.join(author_work_dir, work['id'] + '.json')
+
+	img_small = work['small'] and os.path.join(author_work_dir, work['small'])
+	img_large = work['large'] and os.path.join(author_work_dir, work['large'])
 
 	img_data = {}
 	# TODO: process image data
-
+	if img_large:
+		im = Image.open(img_large)	
+		w, h = im.size
+		
+		colors = list(im.getcolors(w*h))
+		img_data['colors'] = []
+		img_data['r'] = []
+		img_data['g'] = []
+		img_data['b'] = []
+		channels = [{}, {}, {}]
+		for col in colors:
+			#im g_data['colors'].append([
+			# 	col[0], col[1], hexencode(col[1])
+			# ])
+			for i in range(3):
+				count = 1
+				if col[1][i] in channels[i]:
+					count += channels[i][col[1][i]]
+				channels[i][col[1][i]] = count
+		img_data['r'] = list(sorted(channels[0].items()))
+		img_data['g'] = list(sorted(channels[1].items()))
+		img_data['b'] = list(sorted(channels[2].items()))
+		
+	
 	with file(fjson, 'w') as fd:
 		fd.write(json.dumps(img_data))
 exit(0)
+
