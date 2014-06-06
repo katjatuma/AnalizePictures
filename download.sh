4abab192e34b0228a9d5794a1dc5696effc4d089
Ziga Stopinsek <sigi.kajzer@gmail.com>
2014-06-06 11:47:38 +0200

    download and pack scripts

---
+++ download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd image-processing
+./grep_wiki.py -a brugel -w http://en.wikipedia.org/wiki/List_of_paintings_by_Pieter_Bruegel_the_Elder
+./grep_wiki.py -a picasso -p picasso -w http://www.pablopicasso.org/picasso-paintings.jsp
+./grep_wiki.py -a raphael -w http://en.wikipedia.org/wiki/List_of_paintings_by_Raphael
+./grep_wiki.py -a rembrandt -w http://en.wikipedia.org/wiki/List_of_paintings_by_Rembrandt
+./grep_wiki.py -a vermeer -w http://en.wikipedia.org/wiki/List_of_paintings_by_Johannes_Vermeer
+cd ..
