10a436e22320bff1849b706689eaa033e5b5c169
Ziga Stopinsek <sigi.kajzer@gmail.com>
2014-06-06 11:55:34 +0200

    added readme and more verbose download script

--- download.sh
+++ download.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 cd image-processing
+echo 'Downlaod Brugel'
 ./grep_wiki.py -a brugel -w http://en.wikipedia.org/wiki/List_of_paintings_by_Pieter_Bruegel_the_Elder
+
+echo 'Downlaod Picasso'
 ./grep_wiki.py -a picasso -p picasso -w http://www.pablopicasso.org/picasso-paintings.jsp
+
+echo 'Downlaod Raphael'
 ./grep_wiki.py -a raphael -w http://en.wikipedia.org/wiki/List_of_paintings_by_Raphael
+
+echo 'Downlaod Rembrandt'
 ./grep_wiki.py -a rembrandt -w http://en.wikipedia.org/wiki/List_of_paintings_by_Rembrandt
-./grep_wiki.py -a vermeer -w http://en.wikipedia.org/wiki/List_of_paintings_by_Johannes_Vermeer
+
 cd ..
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Install pyquery
+	pip install pyquery # or sudo pip install pyquery if not in a virtualenv
+
+Run download.sh
+	./download.sh
+
+Run sketch
+	./run
